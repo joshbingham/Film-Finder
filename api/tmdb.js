a248ee9f32a78ad89c283c4ed3b21e90e19048d1
Josh Bingham <joshuabingham@hotmail.co.uk>
2025-12-17 14:55:46 +0000

    Add date logic server-side

--- api/tmdb.js
+++ api/tmdb.js
@@ -6,7 +6,17 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: 'TMDB key not set' });
   }
 
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&with_genres=${genre}`;
+  let url = `https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&with_genres=${genre}`;
+
+  if (days) {
+    const today = new Date();
+    const fromDate = new Date();
+    fromDate.setDate(today.getDate() - Number(days));
+
+    const formatDate = (date) => date.toISOString().split('T')[0];
+
+    url += `&primary_release_date.gte=${formatDate(fromDate)}&primary_release_date.lte=${formatDate(today)}`;
+  }
 
   try {
     const response = await fetch(url);
